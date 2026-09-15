9fba8e644c2dc00fd0b224d105704d2df64ee9c9
DebanwitaDutta <debanwitadutta100@gmail.com>
2026-09-15 22:57:56 +0530

    Delete hand_detector.py

--- hand_detector.py
+++
@@ -1,9 +0,0 @@
-from cvzone.HandTrackingModule import HandDetector
-
-# Detect up to 2 hands
-detector = HandDetector(
-    staticMode=False,
-    maxHands=2,
-    detectionCon=0.7,
-    minTrackCon=0.5
-)
